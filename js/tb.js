2cda4f3c65ee60309633aea7ee4cf1b0123f327d
LeomarEs19 <lespanacastillo20@gmail.com>
2023-04-28 10:22:30 -0600

    nuevo

---
+++ js/tb.js
@@ -0,0 +1,21 @@
+function quince(){
+    let resultado = document.getElementById("resultado");
+    let cantidad = document.getElementById("cantidad").value;
+    let valor = document.getElementById("valor").value;
+    let descuento = 0.15 * (valor * cantidad)
+    resultado.innerText = descuento
+}
+function cincuenta(){
+    let resultado = document.getElementById("resultado");
+    let cantidad = document.getElementById("cantidad").value;
+    let valor = document.getElementById("valor").value;
+    let descuento = 0.5 * (valor * cantidad)
+    resultado.innerText = descuento
+}
+function nada(){
+    let resultado = document.getElementById("resultado");
+    let cantidad = document.getElementById("cantidad").value;
+    let valor = document.getElementById("valor").value;
+    let calculo = valor * cantidad
+    resultado.innerText = calculo
+}
